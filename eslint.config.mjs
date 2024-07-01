1c70086e320d109b4da8a80aed4da1b087b7615b
Javi <javi.dev.79@gmail.com>
2024-07-01 22:00:46 +0100

    Fix tests configuration and add first test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,72 +1,81 @@
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import react from "eslint-plugin-react";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-plugin-prettier";
-import reactRefresh from "eslint-plugin-react-refresh";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
+import react from 'eslint-plugin-react'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import prettier from 'eslint-plugin-prettier'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
-});
+})
 
-export default [{
-    ignores: ["**/dist", "**/.eslintrc.cjs"],
-}, ...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
-    "plugin:react/recommended",
-)), {
-    plugins: {
-        react: fixupPluginRules(react),
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
-        prettier,
-        "react-refresh": reactRefresh,
+export default [
+    {
+        ignores: ['**/dist', '**/.eslintrc.cjs']
     },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+    ...fixupConfigRules(
+        compat.extends(
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+            'plugin:react-hooks/recommended',
+            'prettier',
+            'plugin:react/recommended'
+        )
+    ),
+    {
+        plugins: {
+            react: fixupPluginRules(react),
+            '@typescript-eslint': fixupPluginRules(typescriptEslint),
+            prettier,
+            'react-refresh': reactRefresh
         },
 
-        parser: tsParser,
-        ecmaVersion: 2020,
-        sourceType: "module",
-
-        parserOptions: {
-            project: "./tsconfig.json",
-
-            ecmaFeatures: {
-                jsx: true,
+        languageOptions: {
+            globals: {
+                ...globals.browser
             },
+
+            parser: tsParser,
+            ecmaVersion: 2020,
+            sourceType: 'module'
+
+            // parserOptions: {
+            //     project: './tsconfig.json',
+
+            //     ecmaFeatures: {
+            //         jsx: true
+            //     }
+            // }
         },
-    },
 
-    settings: {
-        react: {
-            version: "detect",
+        settings: {
+            react: {
+                version: 'detect'
+            }
         },
-    },
 
-    rules: {
-        "react/react-in-jsx-scope": "off",
-        semi: ["error", "never"],
-        quotes: ["error", "single"],
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            semi: ['error', 'never'],
+            quotes: ['error', 'single'],
 
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
+            'react-refresh/only-export-components': [
+                'warn',
+                {
+                    allowConstantExport: true
+                }
+            ],
 
-        "prettier/prettier": "error",
-    },
-}];
+            'prettier/prettier': 'error'
+        }
+    }
+]
---
+++ tests/Constants.test.tsx
@@ -0,0 +1,16 @@
+import items from '../src/components/Constants'
+import { describe, it, expect, test } from 'vitest'
+
+describe('Constants', () => {
+    it('should export items correctly', () => {
+        expect(items).toBeDefined() // Verify that items are defined
+        expect(Array.isArray(items)).toBeTruthy() // Verify that items is an array
+    })
+
+    it('should contain correct data structure', () => {
+        expect(items.length).toBeGreaterThan(0) // Check that items is not empty
+        expect(items[0]).toHaveProperty('area') // Check that the first element has the 'area' property
+        expect(items[0]).toHaveProperty('companies') // Verify that the first element has the 'companies' property
+        expect(Array.isArray(items[0].companies)).toBeTruthy() // Verify that 'companies' is an array
+    })
+})
