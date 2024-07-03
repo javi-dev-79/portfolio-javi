f072f388a0a5d183008156822302da6c746030d3
Javi <javi.dev.79@gmail.com>
2024-07-03 11:58:06 +0100

    feat: improve carousel styles and make navigation arrows thicker

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,34 +15,36 @@ const rrssIcons = [
     }
 ]
 
-const bgHome = {
-    backgroundImage: `linear-gradient(to top, rgba(30, 35, 38, .8), rgba(30, 35, 38, 1)), url(${bgImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center'
-}
-
 const Header = () => {
     const { t } = useTranslation()
     return (
-        <div id='home' className='mt-20 flex justify-center' style={bgHome}>
-            <div className='bg-custom-dark m-20 pb-10 rounded-xl mb-24 border-2 border-custom-green'>
+        <div
+            id='home'
+            className='min-h-screen flex justify-center items-center bg-cover bg-center'
+            style={{
+                backgroundImage: `linear-gradient(to top, rgba(30, 35, 38, .8), rgba(30, 35, 38, 1)), url(${bgImage})`
+            }}
+        >
+            <div className='bg-custom-grey bg-opacity-90 m-10 pb-10 rounded-xl mb-24 border-2 border-custom-green'>
                 <img
                     src={profilePhoto}
                     alt='image'
                     className='m-10 border-8 border-custom-green block w-4/5 mx-auto rounded-full'
                 />
-                <h1 className='text-5xl font-bold text-center font-righteous'>
+                <h1 className='text-5xl font-bold text-center font-righteous text-white'>
                     JAVIER <br />
                     CORDERO
                 </h1>
-                <h2 className='text-center mb-10'>{t('Programador Junior')}</h2>
+                <h2 className='text-center mb-10 mt-4 text-white'>
+                    {t('Programador Junior')}
+                </h2>
 
                 <div className='flex justify-center items-center m-10 space-x-4'>
                     {rrssIcons.map((rrssIcon, index) => (
                         <div key={index}>
                             <a
                                 href={rrssIcon.iconRef}
-                                className='no-underline border border-white rounded-full w-10 h-10 leading-10 mx-2.5 text-xl transition duration-300 hover:bg-custom-green items-center justify-center flex'
+                                className='no-underline border border-white rounded-full w-10 h-10 leading-10 mx-2.5 text-xl transition duration-300 hover:bg-custom-green text-white flex items-center justify-center'
                             >
                                 {rrssIcon.icon}
                             </a>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,41 +1,97 @@
-import { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { De, Es, Fr, Gb } from 'react-flags-select'
 import { GrLanguage } from 'react-icons/gr'
 
-const navLinks = [
+interface NavLink {
+    text: string
+    itemRef: string
+}
+
+interface Country {
+    name: string
+    flag: JSX.Element
+}
+
+const navLinks: NavLink[] = [
     { text: 'INICIO', itemRef: '#home' },
     { text: 'SOBRE MI', itemRef: '#aboutme' },
     { text: 'SKILLS', itemRef: '#myskills' },
-    { text: 'CURRICULUM', itemRef: '#resume' }
+    { text: 'CURRICULUM', itemRef: '#resume' },
+    { text: 'PROYECTOS', itemRef: '#projects' }
 ]
 
-const countries = [
+const countries: Country[] = [
     { name: 'es', flag: <Es /> },
     { name: 'en', flag: <Gb /> },
     { name: 'fr', flag: <Fr /> },
     { name: 'de', flag: <De /> }
 ]
 
-const NavBar = () => {
+const NavBar: React.FC = () => {
     const { t, i18n } = useTranslation()
-    const [menuOpen, setMenuOpen] = useState(false)
-    const [isOpen, setIsOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState<boolean>(false)
+    const [languageMenuOpen, setLanguageMenuOpen] = useState<boolean>(false)
+
+    const languageMenuRef = useRef<HTMLDivElement>(null)
+    const menuRef = useRef<HTMLDivElement>(null)
+
+    React.useEffect(() => {
+        const handleClickOutside = () => {
+            setMenuOpen(false)
+            setLanguageMenuOpen(false)
+        }
+
+        const handleDocumentClick = (event: MouseEvent) => {
+            const navbar = document.getElementById('navbar')
+
+            if (
+                !event.target ||
+                !(event.target instanceof Element) ||
+                event.target.closest('#navbar') !== navbar
+            ) {
+                handleClickOutside()
+            }
+        }
+
+        document.addEventListener('mousedown', handleDocumentClick)
+
+        return () => {
+            document.removeEventListener('mousedown', handleDocumentClick)
+        }
+    }, [])
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen)
-        setIsOpen(false) // Ensure that the language menu is closed when the main menu is opened
+        setLanguageMenuOpen(false)
+    }
+
+    const toggleLanguageMenu = () => {
+        setLanguageMenuOpen(!languageMenuOpen)
+        setMenuOpen(false)
     }
 
     const changeLanguage = (lng: string) => {
         console.log('Cambiando a idioma:', lng)
         i18n.changeLanguage(lng)
-        setMenuOpen(false) // Close the main menu after changing the language
-        setIsOpen(false) // Close the language menu after changing the language
+        setLanguageMenuOpen(false)
+    }
+
+    const handleNavLinkClick = (
+        event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+        itemRef: string
+    ) => {
+        event.preventDefault()
+        const targetSection = document.querySelector(itemRef)
+        if (targetSection instanceof HTMLElement) {
+            targetSection.scrollIntoView({ behavior: 'smooth' })
+        }
+        setMenuOpen(false)
+        setLanguageMenuOpen(false)
     }
 
     return (
-        <div className='bg-custom-dark text-white'>
+        <div id='navbar' className='bg-custom-dark text-white'>
             <header className='fixed top-0 w-full bg-custom-dark text-white z-50'>
                 <div className='container mx-auto p-4 flex justify-between items-center'>
                     <div className='flex items-center'>
@@ -45,14 +101,17 @@ const NavBar = () => {
                         <div className='relative ml-4 lg:ml-0 top-0.5'>
                             <button
                                 className='text-white'
-                                onClick={() => setIsOpen(!isOpen)}
+                                onClick={toggleLanguageMenu}
                             >
                                 <GrLanguage className='text-custom-green text-xl hover:text-2xl' />
                             </button>
-                            {isOpen && (
-                                <ul className='absolute right-0 mt-2 w-18 bg-custom-dark rounded-lg shadow-lg z-50'>
+                            {languageMenuOpen && (
+                                <div
+                                    ref={languageMenuRef}
+                                    className='absolute right-0 mt-2 w-18 bg-custom-dark rounded-lg shadow-lg z-50 language-menu'
+                                >
                                     {countries.map((country, index) => (
-                                        <li
+                                        <div
                                             key={index}
                                             className='py-2 px-4 hover:bg-custom-green cursor-pointer'
                                         >
@@ -65,23 +124,35 @@ const NavBar = () => {
                                                 {country.flag}
                                                 <span>{country.name}</span>
                                             </button>
-                                        </li>
+                                        </div>
                                     ))}
-                                </ul>
+                                </div>
                             )}
                         </div>
                     </div>
                     <nav
-                        className={`lg:flex items-center lg:space-x-6 ${menuOpen ? 'block' : 'hidden'} lg:block`}
+                        ref={menuRef}
+                        className={`lg:flex items-center lg:space-x-6 ${
+                            menuOpen ? 'block' : 'hidden'
+                        } main-menu`}
                     >
                         <ul
-                            className={`flex flex-col lg:flex-row lg:space-x-6 lg:space-y-0 space-y-4 ${menuOpen ? 'absolute right-0 mt-4 mr-4 p-4 bg-custom-dark rounded-lg z-50' : ''}`}
+                            className={`flex flex-col lg:flex-row lg:space-x-6 lg:space-y-0 space-y-4 ${
+                                menuOpen
+                                    ? 'absolute right-0 mt-4 mr-4 p-4 bg-custom-dark rounded-lg z-50'
+                                    : ''
+                            }`}
                         >
                             {navLinks.map((navLink, index) => (
                                 <li key={index}>
                                     <a
                                         href={navLink.itemRef}
-                                        onClick={toggleMenu}
+                                        onClick={event =>
+                                            handleNavLinkClick(
+                                                event,
+                                                navLink.itemRef
+                                            )
+                                        }
                                         className='hover:underline hover:text-custom-green hover:font-bold'
                                     >
                                         {t(navLink.text)}
@@ -95,7 +166,7 @@ const NavBar = () => {
                         onClick={toggleMenu}
                     >
                         <i
-                            className={`fa-solid fa-${menuOpen ? 'times' : 'bars'} text-lg`}
+                            className={`fa-solid fa-${menuOpen ? 'times' : 'bars'} text-lg cursor-pointer`}
                         >
                             ☰
                         </i>
